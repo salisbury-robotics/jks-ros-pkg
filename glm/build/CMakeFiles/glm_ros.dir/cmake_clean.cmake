eabc980f151a99ff1d4c4307770c4a13770944b3
adamleeper <adamleeper@gmail.com>
2011-05-23 19:26:38 +0000

    initial checkin of glm

---
+++ glm/build/CMakeFiles/glm_ros.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/glm_ros.dir/GLM/glm.o"
+  "CMakeFiles/glm_ros.dir/GLM/glmimg.o"
+  "CMakeFiles/glm_ros.dir/GLM/glm_util.o"
+  "../lib/libglm_ros.pdb"
+  "../lib/libglm_ros.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/glm_ros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
