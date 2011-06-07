0f433029bc221a607edbe244b9c67d7b2a6084f2
nosliwneb <nosliwneb@gmail.com>
2011-06-07 21:08:40 +0000

    filled out get_Joint_Pos function, should give real values now

--- bosch_arm/bosch_arm_control/controller/include/cc.h
+++ bosch_arm/bosch_arm_control/controller/include/cc.h
@@ -21,10 +21,24 @@ namespace constants{
   const unsigned long errFlags  = 0x0; // error flags
   const WORD cntr_chan = CNTR_2B;      // servo loop counter
   const double cnt2mdeg = 0.072;       // 360 motor degrees every 5000 counts
+  const double r1 = 0.062811565304088; // joint 1, turns pers motor turn
+  const double r2 = 0.12524850894632;  // joint 2&3, turns per motor turn
+  const double m2j [16] = {            // Motor to joint position conversion matrix
+       r1, 0.0, 0.0, 0.0,
+      -r1,  r2, 0.0, 0.0,
+      0.0, -r2,  r2, 0.0,
+      0.0, 0.0, 0.0,  r2
+  };
+  const double j2m [16] = {            // Joint to motor position conversion matrix
+       r1,  r2,  r2, 0.0,
+      0.0,  r2,  r2, 0.0,
+      0.0, 0.0,  r2, 0.0,
+      0.0, 0.0, 0.0,  r2
+  };
 
   const double t_max = 0.184;          // N-m - max 100% duty for RE 40
   const double v_for_t_max = 10.0;     // D/A output volts - 10 volts = max torque
-  const double v_lim = 1080;           // motor speed limit, 1080 degrees per second
+  const double v_lim = 2160;           // motor speed limit, 2160 degrees per second
   const double p_err_lim = 360;        // position err limited to 1 motor revolution
 
   const HBD board = 0;
