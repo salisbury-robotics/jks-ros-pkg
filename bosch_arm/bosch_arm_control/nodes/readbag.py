0702032d044f6bd07a2b3ca98f64707fcb1d964c
bridger.zhao <bridger.zhao@9853d438-d33c-9cad-f040-debb384f1bef>
2011-06-20 06:52:33 +0000

    add data

--- bosch_arm/bosch_arm_control/nodes/readbag.py
+++ bosch_arm/bosch_arm_control/nodes/readbag.py
@@ -3,8 +3,11 @@
 import roslib; roslib.load_manifest('bosch_arm_control')
 import rospy
 import rosbag
-bag = rosbag.Bag('data/touch_table.bag')
-f = open('data/touch_table_acc0.txt', 'w')
+import sys
+bag = rosbag.Bag(sys.argv[1])
+strs=sys.argv[1].split('.')
+output=strs[0]+'.txt'
+f = open(output, 'w')
 for topic, msg, t in bag.read_messages(topics=['/bma180']):
     if msg.iChipSelect==0:
         t=msg.header.stamp.secs+msg.header.stamp.nsecs/1e9
