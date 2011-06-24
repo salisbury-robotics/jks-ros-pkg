f51c189846aba6512d5573b2d1175acfb1ce3a00
bridger.zhao <bridger.zhao@9853d438-d33c-9cad-f040-debb384f1bef>
2011-06-24 00:58:20 +0000

    png batch 3

--- bosch_arm/bosch_arm_control/data/acc_plot.m
+++ bosch_arm/bosch_arm_control/data/acc_plot.m
@@ -2,7 +2,7 @@ function acc_plot(fname)
 a=importdata(fname,'\t');
 a(:,1)=a(:,1)-a(1,1);
 figure;
-plot(a(:,1),a(:,4));
-figure;
-periodogram(a(:,4),[],[],100);
+plot(a(:,1),a(:,2));
+%figure;
+%periodogram(a(:,2),[],[],100);
 end
