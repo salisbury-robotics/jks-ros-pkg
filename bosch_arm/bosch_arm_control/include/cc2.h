54b8882172eca9490b3587e08e367f22d41fc065
nosliwneb <nosliwneb@gmail.com>
2011-08-04 21:25:32 +0000

    updated transmission matrix for new motor 2 gearing

--- bosch_arm/bosch_arm_control/include/cc2.h
+++ bosch_arm/bosch_arm_control/include/cc2.h
@@ -21,35 +21,26 @@ namespace constants{
   const unsigned long errFlags  = 0x0; // error flags
   const WORD cntr_chan = CNTR_2B;      // servo loop counter
   const double cnt2mdeg = 0.072;       // 360 motor degrees every 5000 counts
-  const double r1 = 0.062811565304088; // joint 1, turns pers motor turn
-  const double r2 = 0.12524850894632;  // joint 2-3-4, turns per motor turn
-  const double r3 = 15.9206349206349;  // joint 1 motor turns per joint turn
-  const double r4 = 7.98412698412698;  // joint 2-3-4, motor turns per joint turn
-// const double m2j[16] =              // Motor to joint position conversion matrix
-// {
-//   r1, 0.0, 0.0, 0.0,
-//   -r1, 0.0,  r2, 0.0,
-//   0.0, r2,  -r2, 0.0,
-//   0.0, 0.0, 0.0,  r2
-// };
+  const double r1 = 15.9206349206349;  // motor 1, motor turns per joint turn
+  const double r2 = 7.98412698412698;  // motor 2, motor turns per joint turn
+  const double r3 = 15.9206349206349;  // motor 3, motor turns per joint turn
+  const double r4 = 7.98412698412698;  // motor 4, motor turns per joint turn
 
-const double c24=0.15;
+  const double c24=0.15;               // coupling between joints 2 and 4
 
-const double m2j[16] =              // Motor to joint position conversion matrix
-{
-  r1, 0.0, 0.0, 0.0,
-  -r1, 0.0,  r2, 0.0,
-  0.0, r2,  -r2, 0.0,
-  -c24*r1, 0.0, c24*r2,  r2
-};
+  const double m2j[16] ={              // Motor to joint position conversion matrix
+       1/r1,  0.0,    0.0,  0.0,
+      -1/r1,  0.0,   1/r3,  0.0,
+        0.0, 1/r2,  -1/r3,  0.0,
+    -c24/r1,  0.0, c24/r3, 1/r4
+  };
 
-const double j2m [16] =              // Joint to motor position conversion matrix
-{
-  1/r1, 0.0, 0.0, 0.0,
-  1/r2,  1/r2,  1/r2, 0.0,
-  1/r2,  1/r2, 0.0, 0.0,
-  0.0, -c24/r2, 0.0,  1/r2
-};
+  const double j2m [16] ={              // Joint to motor position conversion matrix
+       r1,     0.0, 0.0, 0.0,
+       r2,      r2,  r2, 0.0,
+       r3,      r3, 0.0, 0.0,
+      0.0, -c24*r4, 0.0,  r4
+  };
 
   const double t_max = 0.184;          // N-m - max @ 100% duty for RE 40
   const double v_for_t_max = 10.0;     // D/A output volts - 10 volts = max torque
@@ -66,24 +57,24 @@ const double j2m [16] =              // Joint to motor position conversion matri
   //the offset from the zero position in forward kinematics to the fixer.
   const double q_off[4]={-1.611620+0.130237, 0.0643001+0.0602076, -1.0000682+0.0171557, -2.3054768+0.0103879};
   //const double q_off[4]={-1.611620, 0.0643001, -1.0000682, -2.3054768};
-//   const double gravc[8]={-0.0375042,0.0186818,
-// -0.0305367,-0.620683,
-// -0.0792363,-0.00757456,
-// 0.161804,0.0102816,
-// };
+  //   const double gravc[8]={-0.0375042,0.0186818,
+  // -0.0305367,-0.620683,
+  // -0.0792363,-0.00757456,
+  // 0.161804,0.0102816,
+  // };
   const double gravc[8]={0.0334182,-0.0027833,
--0.0817209,-0.58573,
--0.0818006,-0.0037055,
-0.19276,0.0044142};
+			 -0.0817209,-0.58573,
+			 -0.0818006,-0.0037055,
+			 0.19276,0.0044142};
   const double link[8]=
-  {-0.0359,
-    0.0023,
-   -0.0272,
-   -0.5078,
-   -0.0381,
-    0.0142,
-    0.4815,
-    0.0132
-  };
+    {-0.0359,
+     0.0023,
+     -0.0272,
+     -0.5078,
+     -0.0381,
+     0.0142,
+     0.4815,
+     0.0132
+    };
 }
 #endif
