861bc17c4e52883e72ac3a5f2afac3df1688b486
adamleeper@gmail.com <adamleeper@gmail.com@9853d438-d33c-9cad-f040-debb384f1bef>
2012-05-04 01:12:54 +0000

    first pass at a linear local planner

---
+++ trunk/local_planners/include/local_planners/potential_field_solver.h
@@ -0,0 +1,69 @@
+#ifndef LOCAL_PLANNERS_POTENTIAL_FIELD_H
+#define LOCAL_PLANNERS_POTENTIAL_FIELD_H
+
+#include <planning_scene/planning_scene.h>
+#include <moveit_msgs/GetMotionPlan.h>
+#include <moveit_msgs/MotionPlanDetailedResponse.h>
+#include <planning_interface/planning_interface.h>
+#include <planning_models/conversions.h>
+
+namespace local_planners
+{
+
+class LinearJointStepper : public planning_interface::Planner
+{
+  public:
+    LinearJointStepper() {}
+    virtual ~LinearJointStepper() {};
+
+    /*********************************************************/
+    /// Subclass may implement methods below
+    virtual void init(const planning_models::KinematicModelConstPtr& model) {}
+
+    /// Get a short string that identifies the planning interface
+    virtual std::string getDescription(void) const { return "Adam's potential field planner/solver."; }
+
+    /// Get the names of the known planning algorithms (values that can be filled as planner_id in the planning request)
+    virtual void getPlanningAlgorithms(std::vector<std::string> &algs) const { algs.clear(); }
+
+    /**********************************************************/
+    /// Subclass must implement methods below
+    virtual bool solve(const planning_scene::PlanningSceneConstPtr& planning_scene,
+                       const moveit_msgs::GetMotionPlan::Request &req,
+                       moveit_msgs::GetMotionPlan::Response &res) const;
+
+
+    virtual bool solve(const planning_scene::PlanningSceneConstPtr& planning_scene,
+               const moveit_msgs::GetMotionPlan::Request &req,
+               moveit_msgs::MotionPlanDetailedResponse &res) const
+    {
+      moveit_msgs::GetMotionPlan::Response res2;
+      if (solve(planning_scene, req, res2))
+      {
+        res.trajectory_start = res2.trajectory_start;
+        res.trajectory.push_back(res2.trajectory);
+        res.description.push_back("Linear path.");
+        res.processing_time.push_back(res2.planning_time);
+        return true;
+      }
+      return false;
+    }
+
+    /// Determine whether this plugin instance is able to represent this planning request
+    virtual bool canServiceRequest(const moveit_msgs::GetMotionPlan::Request &req,
+                                   planning_interface::PlannerCapability& capabilities) const
+    {
+      ROS_WARN("This planner is allowing all motion planning requests, and may not be doing the right thing...");
+      return true;
+    }
+
+    /// Request termination, if a solve() function is currently computing plans
+    virtual void terminate(void) const {
+    }
+
+};
+
+
+} // local_planners
+
+#endif // LOCAL_PLANNERS_POTENTIAL_FIELD_H
