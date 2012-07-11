48deb1a2f06427be1ffac67daf694b53124657e7
adamleeper <adamleeper@gmail.com>
2012-07-11 18:59:59 +0000

    adding examples back

--- chai3d-2.1/src/extras/CGlobals.h
+++ chai3d-2.1/src/extras/CGlobals.h
@@ -126,8 +126,8 @@
   //--------------------------------------------------------------------
   // HAPTIC DEVICES
   //--------------------------------------------------------------------
-	#define _ENABLE_CUSTOM_DEVICE_SUPPORT
-	#define _ENABLE_DELTA_DEVICE_SUPPORT
+	//#define _ENABLE_CUSTOM_DEVICE_SUPPORT
+	//#define _ENABLE_DELTA_DEVICE_SUPPORT
 
   // disabled devices
   //#define _ENABLE_PHANTOM_DEVICE_SUPPORT
