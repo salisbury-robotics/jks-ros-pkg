3cc32a2d24f21822011f1934f31c0c036a2bcb84
Anthony.T.Pratkanis@gmail.com <Anthony.T.Pratkanis@gmail.com@9853d438-d33c-9cad-f040-debb384f1bef>
2013-10-29 22:17:43 +0000

    A bunch of stuff for stashing

--- trunk/blast/blast_core/test_actions/pr2_grab_object_cupholder.py
+++
@@ -1,7 +0,0 @@
-
-class BlastPr2GrabObjectCupHolderActionExec(BlastActionExec):
-    def __init__(self, robot, manager, guid):
-        BlastActionExec.__init__(self, robot, manager, guid)
-    def run(self, parameters):
-        self.set_robot_holder("cup-holder", "arbitrary-object", False)
-set_action_exec("pr2-cupholder", "grab-object-cupholder", BlastPr2GrabObjectCupHolderActionExec)
