74cdf211b9a3a4cbf988b6e61a64761f32ac6955
Anthony.T.Pratkanis <anthony.t.pratkanis@gmail.com>
2015-04-06 18:35:51 +0000

    Get rid of old junk in BLAST

--- blast/blast_core/test_actions/pr2__elevator.py
+++
@@ -1,8 +0,0 @@
-
-
-class BlastPr2ElevatorActionExec(BlastActionExec):
-    def __init__(self):
-        BlastActionExec.__init__(self)
-    def run(self, parameters):
-        self.set_location(parameters["outfloor"])
-set_action_exec(BlastPr2ElevatorActionExec)
